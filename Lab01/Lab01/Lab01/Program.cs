22914a0209b36bfcdc8ca6eb48d666da35d4cc56
LelaGraffenreed <35935890+LelaGraffenreed@users.noreply.github.com>
2018-02-03 19:00:44 -0500

    Lab01

---
+++ Lab01/Lab01/Lab01/Program.cs
@@ -0,0 +1,14 @@
+﻿//Lab01
+//Text -displaying app.
+using System;
+
+class Lela
+{
+    //Main method begins execution of C# app
+    static void Main()
+    {
+        Console.WriteLine("Hello World From Lela");
+    }// end Main
+}//end class Lela
+
+
